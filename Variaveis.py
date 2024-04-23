9fa16780c8d7181ca077d4f968e7cc9ebed12ea8
LucasD14S <lucasdiasribeiro2007@gmail.com>
2024-04-23 16:40:31 -0300

    criação variaveis.py

---
+++ Variaveis.py
@@ -0,0 +1,12 @@
+nome="Lucas Dias"
+idade=16
+email='lucasdiasribeiro2007@gmail.com'
+telefone='17 99456-8762'
+nota=8.99
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
